9adb76cac6ecacecba2f1f99b53db44d7be93c38
Rahul Pugal <rahul.pugal@mountblue.io>
2018-06-28 16:40:08 +0530

    register form

--- movie_buffs_ui/src/Components/JS/App.js
+++ movie_buffs_ui/src/Components/JS/App.js
@@ -1,133 +1,135 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from '../ICONS/logo.svg';
+import {login, register} from '../../MoviesBuffsApi.js';
+import '../CSS/App.css';
 
 export default class App extends Component {
     render() {
         const element = <div>
-                            <Home />
+                            <Register />
                         </div>
         return element
     }
 }
 
-class Home extends Component {
+class Login extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            email: "",
+            login: ""
+        }
+        this.handleEmailChange = this.handleEmailChange.bind(this)
+        this.handlePasswordChange = this.handlePasswordChange.bind(this)
+        this.handleLogin = this.handleLogin.bind(this)
+    }
+
+    handleEmailChange(event) {
+        this.setState({
+            email: event.target.value
+        })
+    }
+
+    handlePasswordChange(event) {
+        this.setState({
+            password: event.target.value
+        })
+    }
+
+    handleLogin() {
+        login(this.state.email, this.state.password)
+        .then((user) => {
+            if(user.error) {
+                alert(user.error)
+            }
+            else {
+                alert('login successfull')
+                let sessionDetails = {
+                    'localId': user.localId,
+                    'refreshToken': user.refreshToken
+                }
+                localStorage.setItem('sessionDetails', JSON.stringify(sessionDetails))
+            }
+        })
+    }
+ 
     render() {
-        const element = <div>
-                            <Header />
-                            <div className='container-fluid dark-bg'>
-                                <div className='container'>
-                                    <Carousel />
-                                </div>
-                            </div>
-                            <div className='container-fluid light-bg'>
-                                <div className='container'>
-                                    <MovieShelf />
-                                </div>
-                            </div>
-                        </div>
+        const element = <form>
+                            <input type="text" placeholder="Email" value={this.state.email} 
+                            onChange={this.handleEmailChange} />
+                            <input type="password" placeholder="Password" 
+                            value={this.state.password} onChange={this.handlePasswordChange}/>
+                            <button type="button" onClick={this.handleLogin}>Login</button>
+                        </form>
         return element
     }
 }
 
-class Header extends Component {
-  render() {
-    return (
-      <div className="Search-container">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="Header-text">movieBuffs</h1>
-          <Logout />
-          <Search />
-        </header>
-      </div>
-    );
-  }
-}
+class Register extends Component {
 
-class Search extends Component {
-  render() {
-    return (
-        <input
-          className="quick-search-input"
-          value="Quick Search"
-          type="search"
-        />
-    );
-  }
-}
-
-class Logout extends Component {
-  render() {
-    return (
-      <button type="button" class="logout-btn">
-        Logout
-      </button>
-    );
-  }
-}
-
-class Carousel extends Component {
-    render() {
-        const element = <div id="demo" className="carousel slide" data-ride="carousel">
-                            <ul className="carousel-indicators">
-                                <li data-target="#demo" data-slide-to="0" className="active"></li>
-                                <li data-target="#demo" data-slide-to="1"></li>
-                                <li data-target="#demo" data-slide-to="2"></li>
-                            </ul>
-                            <div className="carousel-inner">
-                                <div className="carousel-item active">
-                                    <MovieGrid />
-                                </div>
-                                <div className="carousel-item">
-                                    <MovieGrid />
-                                </div>
-                                <div className="carousel-item">
-                                    <MovieGrid />
-                                </div>
-                            </div>
-                            <a className="carousel-control-prev" href="#demo" data-slide="prev">
-                                <span className="carousel-control-prev-icon"></span>
-                            </a>
-                            <a className="carousel-control-next" href="#demo" data-slide="next">
-                                <span className="carousel-control-next-icon"></span>
-                            </a>
-                        </div>
-        return element
+    constructor() {
+        super()
+        this.state = {
+            firstName: '',
+            lastName: '',
+            age: '',
+            email: '',
+            password: '',
+        } 
+        this.handleFirstNameChange = this.handleFirstNameChange.bind(this)
+        this.handleLastNameChange = this.handleLastNameChange.bind(this)
     }
-}
 
-class MovieShelf extends Component {
-    render() {
-        const element = <div>
-                            <h3>Recomended Movies</h3> 
-                            <MovieGrid /> 
-                        </div>
-        return element
+    handleFirstNameChange(event) {
+        this.setState({
+            firstName: event.target.value
+        })
     }
-}
 
-class MovieGrid extends Component {
-    render() {
-        const element = <div>
-                            <div className='row'>
-                                <MoviePoster />
-                                <MoviePoster />
-                                <MoviePoster />
-                                <MoviePoster />
-                            </div>
-                        </div>
-        return element
+    handleLastNameChange(event) {
+        this.setState({
+            firstName: event.target.value
+        })
     }
-}
 
-class MoviePoster extends Component {
+    handleAgeChange(event) {
+        this.setState({
+            age: event.target.value
+        })
+    }
+
+    handleEmailChange(event) {
+        this.setState({
+            email: event.target.value
+        })
+    }
+
+    handlePasswordChange(event) {
+        this.setState({
+            password: event.target.value
+        })
+    }
+
+    handleRegister() {
+
+    }
+
+
     render() {
-        const element = <div className='col-md-3 col-sm-6 movie'>
-                            <div className='movie-cover-conatiner'>
-                                <img className='movie-cover' src='http://image.tmdb.org/t/p/original/tvSlBzAdRE29bZe5yYWrJ2ds137.jpg' alt='' />
-                            </div>
-                        </div>
+        const element = <form>
+                            <input type="text" placeholder="First Name" 
+                            value={this.state.firstName} onChange={this.handleFirstNameChange} />
+                            <input type="text" placeholder="Last Name"
+                            value={this.state.lastName} onChange={this.handleLastNameChange} />
+                            <input text="text" placeholder="Age"
+                            value={this.state.age} onChange={this.handleAgeChange} />
+                            <input type="text" placeholder="Email" value={this.state.email} 
+                            onChange={this.handleEmailChange} />
+                            <input type="password" placeholder="Password" 
+                            value={this.state.password} onChange={this.handlePasswordChange} />
+                            <button type="button" onClick={this.handleRegister}>Register</button>
+                        </form>
         return element
     }
 }
