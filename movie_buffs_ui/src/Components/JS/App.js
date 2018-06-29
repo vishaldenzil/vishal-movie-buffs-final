55fb212626ae3b4056ddde95d8cc2e7186f96c00
Rahul Pugal <rahul.pugal@mountblue.io>
2018-06-29 14:00:09 +0530

    ref

--- movie_buffs_ui/src/Components/JS/App.js
+++ movie_buffs_ui/src/Components/JS/App.js
@@ -1,14 +1,22 @@
-import React, { Component } from "react";
-import "../CSS/App.css";
-import Home from "./Home.js";
+import React, {Component} from 'react';
+import '../CSS/App.css';
+import  {BrowserRouter, Route, Switch} from 'react-router-dom'
+import Login from './Login.js';
+import Register from './Register.js';
+import Home from './Home.js'
+import Description from './Description.js'
+
 
 export default class App extends Component {
-  render() {
-    const element = (
-      <div>
-        <Home />
-      </div>
-    );
-    return element;
-  }
+    render() {
+        const element = <BrowserRouter>
+                            <Switch>
+                                <Route path='/' component={Login} exact />
+                                <Route path='/register' component={Register} exact />
+                                <Route path='/home'  component={Home} exact />
+                                <Route path='/desc' component={Description} exact />
+                            </Switch>
+                        </BrowserRouter>
+        return element
+    }
 }
--- movie_buffs_ui/src/Components/JS/Body.js
+++ movie_buffs_ui/src/Components/JS/Body.js
@@ -4,25 +4,31 @@ import Carousel from "./Carousel.js";
 import MovieShelf from "./MovieShelf.js";
 
 class Body extends Component {
-  render() {
-    return (
-      <div className="main-container">
-        <div className="container-fluid dark-bg">
-          <div className="container dark-bg">
-            <Carousel />
-          </div>
-        </div>
-        <div className="container-fluid light-bg">
-          <div className="text-container">
-            <h3 className="recomended-text">Recomended Movies</h3>
-          </div>
-          <div className="container size">
-            <MovieShelf />
-          </div>
-        </div>
-      </div>
-    );
-  }
+    
+    constructor() {
+        super()
+
+    }
+
+    render() {
+        return (
+            <div className="main-container">
+                <div className="container-fluid dark-bg">
+                    <div className="container dark-bg">
+                        <Carousel />
+                    </div>
+                </div>
+                <div className="container-fluid light-bg">
+                    <div className="text-container">
+                        <h3 className="recomended-text">Recomended Movies</h3>
+                    </div>
+                    <div className="container size">
+                        <MovieShelf />
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default Body;
