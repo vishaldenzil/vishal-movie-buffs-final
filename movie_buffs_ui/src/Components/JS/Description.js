6ce0edf01b1087777df1d27a8278c328e88df7d3
dev <sandeep.kumar@mountblue.io>
2018-06-30 17:59:27 +0530

    switching to login branch

--- movie_buffs_ui/src/Components/JS/Description.js
+++ movie_buffs_ui/src/Components/JS/Description.js
@@ -1,76 +1,75 @@
-import React, { Component } from 'react';
-import '../CSS/Description.css';
-import Iframe from 'react-iframe'
-import {searchId} from '../../MoviesBuffsApi'
-export default class Description extends Component
-{
-    constructor()
-    {
-       super();
-       this.state={
-              Movies:{}
-              };
+import React, { Component } from "react";
+import "../CSS/Description.css";
+import Iframe from "react-iframe";
+import { searchId } from "../../MoviesBuffsApi";
+import Header from "./Header.js";
 
-     this.getMovie=this.getMovie.bind(this);
-    }
+export default class Description extends Component {
+  constructor() {
+    super();
+    this.state = {
+      Movies: {}
+    };
 
-    getMovie()
-    {
-       let id=this.props.match.params.id;
-       searchId(id)
-         .then((movie) => {
-             console.log(movie.Actors);
-           this.setState({Movies:movie})
-         });
-    }
+    this.getMovie = this.getMovie.bind(this);
+  }
 
-    componentDidMount()
-    {
-       this.getMovie();
-    }
-    render()
-    {
-        let data=this.state;
-        return(
-            
-            <div>
-                <div className="row-first">
-                    <div className="column-1">
-                       <img  src={data.Movies.movie && data.Movies.movie.Poster} />
-                    </div>
-                    <div className="column-2">
-                     <div class="row-1">
-                       <h3>{data.Movies.movie && data.Movies.movie.Title}</h3>
-                       <h4>{data.Movies.movie && data.Movies.movie.Year}</h4>
-                       <h6>{data.Movies.movie && data.Movies.movie.Genre}</h6>
-                     </div>
-                    <div>
-                        <h6> Ratings  : {data.Movies.movie && data.Movies.movie.imdbRating}</h6>
-                     </div>
-                    <div> 
-                     <h4> Plot </h4>
-                     <p>
-                    {data.Movies.movie && data.Movies.movie.Plot}
-                    </p>
-                     </div>
-                    </div>
-                </div>
+  getMovie() {
+    let id = this.props.match.params.id;
+    searchId(id).then(movie => {
+      console.log(movie.Actors);
+      this.setState({ Movies: movie });
+    });
+  }
 
-                <div class="iframe">
-
-                  <Iframe url={data.Movies.movie && data.Movies.movie.trailer}
-                    width="100%"
-                    height="350px"
-                    id="myId"
-                    className="myClassname"
-                    display="initial"
-                    position="relative"
-                    frameborder="0"
-                    allowFullScreen
-                    ng-show="showvideo"/>
-                </div>
+  componentDidMount() {
+    this.getMovie();
+  }
+  render() {
+    let data = this.state;
+    return (
+      <div>
+        <Header components={{logout:true, search: true, browseMovies: true}} />
+        <div className="main-description-container">
+          <div className="row-first">
+            <div className="column-1">
+              <img src={data.Movies.movie && data.Movies.movie.Poster} />
             </div>
+            <div className="column-2">
+              <div class="row-1">
+                <h3>{data.Movies.movie && data.Movies.movie.Title}</h3>
+                <h4>{data.Movies.movie && data.Movies.movie.Year}</h4>
+                <h6>{data.Movies.movie && data.Movies.movie.Genre}</h6>
+              </div>
+              <div>
+                <h6>
+                  {" "}
+                  Ratings : {data.Movies.movie && data.Movies.movie.imdbRating}
+                </h6>
+              </div>
+              <div>
+                <h4> Plot </h4>
+                <p>{data.Movies.movie && data.Movies.movie.Plot}</p>
+              </div>
+            </div>
+          </div>
 
-        );
-    }
+          <div class="iframe">
+            <Iframe
+              url={data.Movies.movie && data.Movies.movie.trailer}
+              width="100%"
+              height="350px"
+              id="myId"
+              className="myClassname"
+              display="initial"
+              position="relative"
+              frameborder="0"
+              allowFullScreen
+              ng-show="showvideo"
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
