3c7bf24991c4c164ca7f0fd6872319fe33ee0707
dev <sandeep.kumar@mountblue.io>
2018-06-30 11:06:48 +0530

    final version of login and signup form for MVP

--- movie_buffs_ui/src/Components/JS/Login.js
+++ movie_buffs_ui/src/Components/JS/Login.js
@@ -48,47 +48,52 @@ export default class Login extends Component {
       <div>
         <Header />
         <div className="main-login-container">
-          <div className="login-container">
-            <form>
-              <div class="input-group email">
-                <span class="input-group-addon">
-                <i class="far fa-user"></i>
-                </span>
-                <input
-                  className="form-control height-width"
-                  type="text"
-                  placeholder="Email"
-                  value={this.state.email}
-                  onChange={this.handleEmailChange}
-                />
-              </div>
-              <div class="input-group password">
-                <span class="input-group-addon">
-                <i class="fas fa-lock"></i>
-                </span>
-                <input
-                  className="form-control height-width"
-                  type="password"
-                  placeholder="Password"
-                  value={this.state.password}
-                  onChange={this.handlePasswordChange}
-                />
-              </div>
-              <button type="button" onClick={this.handleLogin}>
-                Login
-              </button>
-            </form>
-            <hr className="line-sapration"/>
-            <div className="online-login">
-            <h5 className="social-login-text">Login with Social</h5>
-                <div className="google-login">
+          <div className="main-helper-container">
+            <div className="login-container">
+              <form>
+                <div class="input-group email">
+                  <span class="input-group-addon">
+                    <i class="far fa-user" />
+                  </span>
+                  <input
+                    className="form-control height-width"
+                    type="text"
+                    placeholder="Email"
+                    value={this.state.email}
+                    onChange={this.handleEmailChange}
+                  />
                 </div>
-                <div className="fb-login">
+                <div class="input-group password">
+                  <span class="input-group-addon">
+                    <i class="fas fa-lock" />
+                  </span>
+                  <input
+                    className="form-control height-width"
+                    type="password"
+                    placeholder="Password"
+                    value={this.state.password}
+                    onChange={this.handlePasswordChange}
+                  />
                 </div>
+                <button type="button" onClick={this.handleLogin}>
+                  Login
+                </button>
+              </form>
+              <hr className="line-sapration" />
+              <div className="online-login">
+                <h5 className="social-login-text">Login with Social</h5>
+                <div className="google-login" />
+                <div className="fb-login" />
+              </div>
+              <hr className="line-sapration" />
+              <h5 className="register-text">
+                New to MovieBuffs ?{" "}
+                <code>
+                  <NavLink to="/register">register here...<i class="fas fa-user-plus"></i></NavLink>
+                </code>
+              </h5>
             </div>
-            <hr className="line-sapration"/>
-            <h5 className="register-text">New to MovieBuff ?  <code><NavLink to="/register">register here...</NavLink></code></h5>
-            </div>
+          </div>
         </div>
       </div>
     );
