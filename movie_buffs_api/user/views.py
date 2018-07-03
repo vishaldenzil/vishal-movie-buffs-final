7e8fe511618a90a29732204d3bdc28f5789130a2
Rahul Pugal <rahul.pugal@mountblue.io>
2018-07-03 11:26:06 +0530

    null

--- movie_buffs_api/user/views.py
+++ movie_buffs_api/user/views.py
@@ -50,12 +50,9 @@ def get_user_movies(request):
         user_id = request.GET.get('user_id', '')
         try:
             user_movies = db.child('users').child(user_id).child('movies').get().val()
-            print(user_movies, 'user movies')
             movies = list()
             for movie in user_movies:
                 movies.append(db.child('movies').child(movie).get().val())
-            print('------------------------------------------------')
-            print(movies, 'movies')
             return JsonResponse(movies, status=status.HTTP_200_OK, safe=False)
         except Exception as e:
             return JsonResponse(e, status=status.HTTP_400_BAD_REQUEST, safe=False)
