8190f02269996a0979152eb30e7a681dc83b0cb0
dev <sandeep.kumar@mountblue.io>
2018-07-07 22:46:55 +0530

    user reviews is done, except review add button

--- movie_buffs_ui/src/Components/JS/Reviews.js
+++ movie_buffs_ui/src/Components/JS/Reviews.js
@@ -3,66 +3,79 @@ import { addReview } from "../../MoviesBuffsApi.js";
 import "../CSS/Reviews.css";
 
 export default class Reviews extends Component {
-	constructor() {
-	    super();
-	    this.state = {
-	    	reviews: [],
-	    	imdbId: ''
-	    };
-	    this.addReviewItem = this.addReviewItem.bind(this)
-	}
+    constructor() {
+        super();
+        this.state = {
+            reviews: [],
+            imdbId: ''
+        };
+        this.addReviewItem = this.addReviewItem.bind(this)
+    }
 
-	addReviewItem(event) {
-		event.preventDefault()
-		let reviewData = {
-			user_id: JSON.parse(localStorage.sessionDetails).localId,
-			imdb_id: this.props.imdbId,
-			text: event.target.reviewText.value
-		}
-		addReview(reviewData) 
-		.then(() => {
-			this.props.method();
-		})
-	}
+    addReviewItem(event) {
+        event.preventDefault()
+        let reviewData = {
+            user_id: JSON.parse(localStorage.sessionDetails).localId,
+            imdb_id: this.props.imdbId,
+            text: event.target.reviewText.value
+        }
+        addReview(reviewData)
+            .then(() => {
+                this.props.method();
+            })
+    }
 
-	componentWillReceiveProps(nextProps) {
-		this.setState ({
-			reviews: Object.values(nextProps.reviews),
-  			imdbId: nextProps.imdbId
-		})
-	}
+    componentWillReceiveProps(nextProps) {
+        this.setState({
+            reviews: Object.values(nextProps.reviews),
+            imdbId: nextProps.imdbId
+        })
+    }
 
-  	componentDidMount() {
-  		this.setState({
-  			reviews: Object.values(this.props.reviews),
-  			imdbId: this.props.imdbId
-  		})
-  	}
+    componentDidMount() {
+        this.setState({
+            reviews: Object.values(this.props.reviews),
+            imdbId: this.props.imdbId
+        })
+    }
 
-  	render() {
-  		console.log(this.state.call)
-  		console.log(this.props.reviews)
-    	const element = <div>
-    						<div className="text-container">
-                            	<h3 className="recomended-text">Reviews</h3>
-                        	</div>
-    						<div>
-    							<form onSubmit={this.addReviewItem}>
-    								<input type='text' name='reviewText'/>
-    								<button type='submit' />
-    							</form>
-    						</div>
-    						{this.state.reviews.map((review) => {
-    							return 	<div>
-    										<div>
-    											<i class="far fa-user"/>
-    											<span>{review.user_name}</span>
-    										</div>
-    										<div>{review.review}</div>
-    										<br/>
-    									</div>
-    						})}
-    					</div>
-    	return element;
-  	}
+    render() {
+        const element = <div className="main-review-container">
+            <div className="tcomment-text">
+                <h3><i class="fas fa-comment-alt"></i>&nbsp;Comments</h3>
+            </div>
+            <div className="comments-scroll">
+                {this.state.reviews.map((review) => {
+                    return <Comment review={review} />
+                })}
+            </div>
+            <div>
+                <form onSubmit={this.addReviewItem}>
+                    <input type='text' name='reviewText' />
+                    <button type='submit' />
+                </form>
+            </div>
+
+        </div>
+        return element;
+    }
+}
+
+
+
+
+class Comment extends Component {
+    render() {
+        const element = <div className="comment">
+            <a href="#" className="avatar-thumd">
+                <img className="avatar-img" alt="yasirjunejo profile" src="https://yts.am/assets/images/users/thumb/15258121502816204.jpg" />
+            </a>
+            <div className="comment-text">
+                <h5>{this.props.review.user_name}</h5>
+                <p>{this.props.review.review}</p>
+                <br />
+            </div>
+        </div>
+        return element;
+    }
 }
